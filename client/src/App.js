16c46cdaba4546f27b0253030ecb96cfc8093258
Dias Mukash <diasmukash01@gmail.com>
2023-04-21 14:01:10 +0300

    App.js, App.css, index.js fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
 import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
 import Footer from './components/Footer/Footer';
-
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/signin" element={<SignIn />} />
-          <Route exact path="/signup" element={<SignUp />} />
-        </Routes>
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/signin" element={<SignIn />} />
+            <Route exact path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
+        {/* <HomePage /> */}
         <Footer />
-      </BrowserRouter>
     </div>
   );
 }
