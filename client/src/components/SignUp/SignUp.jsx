b0fb5b11df0336c1d0b271ee77a17dbe50e20f72
Dias Mukash <diasmukash01@gmail.com>
2023-04-30 22:46:06 +0300

    SignIn and SignUp components fix

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,23 +1,10 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
+import React, { useState } from 'react';
+import { Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-
-const theme = createTheme();
 
 export default function SignUp() {
-  const [role, setRole] = React.useState("Applicant");
+  const [role, setRole] = useState("Applicant");
 
   const handleRoleChange = (event, newRole) => {
     if (newRole !== null) {
@@ -35,117 +22,116 @@ export default function SignUp() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 0,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+
+        <ToggleButtonGroup
+          value={role}
+          exclusive
+          onChange={handleRoleChange}
+          sx={{ mt: 2, mb: 2 }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
+          <ToggleButton value="Applicant" sx={{ bgcolor: role === 'Applicant' ? 'primary.main' : '', color: role === 'Applicant' ? 'common.white' : '' }}>
+            Applicant
+          </ToggleButton>
+          <ToggleButton value="Employer" sx={{ bgcolor: role === 'Employer' ? 'primary.main' : '', color: role === 'Employer' ? 'common.white' : '' }}>
+            Employer
+          </ToggleButton>
+        </ToggleButtonGroup>
 
-          <ToggleButtonGroup
-            value={role}
-            exclusive
-            onChange={handleRoleChange}
-            sx={{ mt: 2, mb: 2 }}
-          >
-            <ToggleButton value="Applicant" sx={{ bgcolor: role === 'Applicant' ? 'primary.main' : '', color: role === 'Applicant' ? 'common.white' : '' }}>
-              Applicant
-            </ToggleButton>
-            <ToggleButton value="Employer" sx={{ bgcolor: role === 'Employer' ? 'primary.main' : '', color: role === 'Employer' ? 'common.white' : '' }}>
-              Employer
-            </ToggleButton>
-          </ToggleButtonGroup>
-
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              {role === "Applicant" ? (
-                <>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      autoComplete="given-name"
-                      name="firstName"
-                      required
-                      fullWidth
-                      id="firstName"
-                      label="First Name"
-                      autoFocus
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="lastName"
-                      label="Last Name"
-                      name="lastName"
-                      autoComplete="family-name"
-                    />
-                  </Grid>
-                </>
-              ) : (
-                <Grid item xs={12}>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            {role === "Applicant" ? (
+              <>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+              </>
+            ) : (
+              <Grid item xs={12}>
                 <TextField
+                  required
+                  fullWidth
+                  id="companyName"
+                  label="Company Name"
+                  name="companyName"
+                  autoComplete="organization"
+                />
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <TextField
                 required
                 fullWidth
-                id="companyName"
-                label="Company Name"
-                name="companyName"
-                autoComplete="organization"
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
               />
             </Grid>
-          )}
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-            />
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="new-password"
-            />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            >
+            Sign Up
+          </Button>
+          <Grid container>
+            <Grid item>
+              <RouterLink to="/signin" style={{ textDecoration: 'none' }}>
+                <Link variant="body2">
+                  {"Already have an account? Sign in"}
+                </Link>
+              </RouterLink>
+            </Grid>
           </Grid>
-        </Grid>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign Up
-        </Button>
-        <Grid container>
-          <Grid item>
-            <RouterLink to="/signin" style={{ textDecoration: 'none' }}>
-              <Link variant="body2">
-                {"Already have an account? Sign in"}
-              </Link>
-            </RouterLink>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
-  </Container>
-</ThemeProvider>);
+    </Container>
+);
 }
