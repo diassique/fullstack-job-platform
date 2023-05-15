dd71f85fcf1de4f1ccb2be47b2c0070804a3da4d
Dias Mukash <diasmukash01@gmail.com>
2023-05-16 02:41:50 +0300

    fixed image displaying

--- client/src/components/common/Homepage.jsx
+++ client/src/components/common/Homepage.jsx
@@ -5,16 +5,25 @@ import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Link as RouterLink } from 'react-router-dom';
+import contentImageFirst from '../../assets/pic-1.svg';
+import contentImageSecond from '../../assets/pic-2.svg';
+import contentImageThird from '../../assets/pic-3.svg';
+import styled from 'styled-components';
 
-export default function HomePage() {
+const ContentImage = styled('img')`
+  width: 60%;
+  margin-bottom: 1rem;
+`;
+
+export default function Homepage() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       {/* Hero section */}
       <Box sx={{ minHeight: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5' }}>
-      <Typography variant="h2" component="h1" gutterBottom sx={{ textAlign: 'center', px: { xs: 2, sm: 0 } }}>
+        <Typography variant="h2" component="h1" gutterBottom sx={{ textAlign: 'center', px: { xs: 2, sm: 0 } }}>
           Online Recruitment System
         </Typography>
-        <Typography variant="h5" component="p" gutterBottom>
+        <Typography variant="h5" component="p" gutterBottom sx={{ textAlign: 'center', px: { xs: 2, sm: 0 } }}>
           Find your dream job or hire the best talent
         </Typography>
         <Box mt={4}>
@@ -35,7 +44,7 @@ export default function HomePage() {
         <Grid container spacing={4} justifyContent="center">
           <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={3} sx={{ p: 4, minHeight: '250px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', borderRadius: '20px' }}>
-              <img src={`${process.env.PUBLIC_URL}/assets/images/pic-1.svg`} alt="Job Positions" style={{ width: '50%', marginBottom: '1rem' }} />
+              <ContentImage src={contentImageFirst} alt="Job Positions"/>
               <Typography variant="h6" component="h3">
                 Wide Range of Job Positions
               </Typography>
@@ -46,7 +55,7 @@ export default function HomePage() {
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={3} sx={{ p: 4, minHeight: '250px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', borderRadius: '20px' }}>
-              <img src={`${process.env.PUBLIC_URL}/assets/images/pic-2.svg`} alt="Recruitment Process" style={{ width: '50%', marginBottom: '1rem' }} />
+              <ContentImage src={contentImageSecond} alt="Recruitment Process"/>
               <Typography variant="h6" component="h3">
                 Efficient Recruitment Process
               </Typography>
@@ -57,7 +66,7 @@ export default function HomePage() {
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
             <Paper elevation={3} sx={{ p: 4, minHeight: '250px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', borderRadius: '20px' }}>
-              <img src={`${process.env.PUBLIC_URL}/assets/images/pic-3.svg`} alt="Candidate Profiles" style={{ width: '50%', marginBottom: '1rem' }} />
+              <ContentImage src={contentImageThird} alt="Candidate Profiles"/>
               <Typography variant="h6" component="h3">
                 Comprehensive Candidate Profiles
               </Typography>
