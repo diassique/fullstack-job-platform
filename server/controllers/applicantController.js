feaa674ec4d0d62679700b432ec14a1a29e367e6
Dias Mukash <diasmukash01@gmail.com>
2023-05-22 16:07:18 +0300

    added avatar uploading for applicant

--- server/controllers/applicantController.js
+++ server/controllers/applicantController.js
@@ -66,6 +66,78 @@ exports.updateUserDetails = async (req, res) => {
   }
 };
 
+// Avatar upload
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => {
+    const dir = path.join(__dirname, `../uploads/applicants/${req.user.id}`);
+    await fs.promises.mkdir(dir, { recursive: true });
+    console.log('Created directory:', dir);
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, 'avatar' + path.extname(file.originalname));
+  },
+});
+
+exports.upload = multer({ 
+  storage: storage,
+  fileFilter: function(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      console.log('Invalid file type:', file.originalname);
+      return cb(new Error('Please upload an image in jpg, jpeg or png format.'));
+    }
+    console.log('Valid file type:', file.originalname);
+    cb(null, true);
+  },
+});
+
+exports.uploadAvatar = async (req, res) => {
+  try {
+    const user = await Applicant.findById(req.user.id);
+    const newAvatarPath = req.file.filename; // use filename instead of path
+    const userDir = path.join(__dirname, `../uploads/applicants/${req.user.id}`);
+    const files = await fs.promises.readdir(userDir);
+
+    for (const file of files) {
+      const filePath = path.join(userDir, file);
+      if (filePath !== path.join(userDir, newAvatarPath)) {
+        await fs.promises.unlink(filePath);
+      }
+    }
+
+    user.avatar = newAvatarPath;
+    await user.save();
+    res.json({ avatar: user.avatar });
+  } catch (error) {
+    console.log('Error in uploadAvatar:', error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.deleteAvatar = async (req, res) => {
+  try {
+    const user = await Applicant.findById(req.user.id);
+    if (user.avatar) {
+      const avatarPath = path.join(__dirname, `../uploads/applicants/${req.user.id}/${user.avatar}`);
+      fs.unlink(avatarPath, (err) => {
+        if (err) return res.status(500).json({ error: err.message });
+        user.avatar = undefined;
+        user.save()
+        .then(() => {
+          res.json({ message: 'Avatar deleted' });
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.message });
+        });
+      });
+    } else {
+      res.json({ message: 'No avatar to delete' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 exports.register = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
